f8115e0f71250e6732ee5a9fc924971d75e8a55c
Matheus V. <matheusvictorbarbosasoares@gmail.com>
2026-05-10 18:35:10 -0300

    Add files via upload

---
+++ RachaAi/Models/Grupo.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RachaAi.Models
+{
+    public class Grupo
+    {
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
